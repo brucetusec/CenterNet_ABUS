49b2debb55d36047d52c4d1dbffd4455d039eab3
r07922097 <r07922097@ntu.edu.tw>
2020-01-15 17:51:42 +0800

    Generate hm, hw_xyz, and draw them along y axis.

--- src/data/heatmap.py
+++ src/data/heatmap.py
@@ -4,15 +4,7 @@ def _embed_matrix(A, B, start_point):
     A[start_point[0]:start_point[0]+B.shape[0], start_point[1]:start_point[1]+B.shape[1], start_point[2]:start_point[2]+B.shape[2]] += B
     return A
 
-# Create a 2D numpy array simulating 2D gaussian dist.
-def gaussian2D(shape, sigma=1):
-    m, n = [(ss - 1.) / 2. for ss in shape]
-    y, x = np.ogrid[-m:m+1,-n:n+1]
-
-    h = np.exp(-(x * x + y * y) / (2 * sigma * sigma))
-    h[h < np.finfo(h.dtype).eps * h.max()] = 0
-    return h
-
+# Create a 3D numpy array simulating 3D gaussian dist.
 def gaussian3D(shape, sigma=1):
     r3, r2, r1 = [(ss - 1.) / 2. for ss in shape]
     z, y, x = np.ogrid[-r3:r3+1,-r2:r2+1,-r1:r1+1]
@@ -37,3 +29,30 @@ def gen_3d_heatmap(size, gt_boxes, scale=1):
         hm = np.maximum(hm, layer)
 
     return hm
+
+# 
+def gen_3d_hw(size, gt_boxes, scale=1):
+    size = [w//scale for w in size]
+    hw_x = np.zeros(size, dtype=np.float32)
+    hw_y = np.zeros(size, dtype=np.float32)
+    hw_z = np.zeros(size, dtype=np.float32)
+
+    for bbox in gt_boxes:
+        shape = (int((bbox['z_range'])//scale), int((bbox['y_range'])//scale), int((bbox['x_range'])//scale))
+        start_point = (int(bbox['z_bot']//scale), int(bbox['y_bot']//scale), int(bbox['x_bot']//scale ))
+        layer = np.zeros(size, dtype=np.float32)
+        chunk = np.full(shape, shape[2], dtype=np.float32)
+        layer = _embed_matrix(layer, chunk, start_point)
+        hw_x = np.maximum(hw_x, layer)
+
+        layer = np.zeros(size, dtype=np.float32)
+        chunk = np.full(shape, shape[1], dtype=np.float32)
+        layer = _embed_matrix(layer, chunk, start_point)
+        hw_y = np.maximum(hw_y, layer)
+
+        layer = np.zeros(size, dtype=np.float32)
+        chunk = np.full(shape, shape[0], dtype=np.float32)
+        layer = _embed_matrix(layer, chunk, start_point)
+        hw_z = np.maximum(hw_z, layer)
+
+    return hw_x, hw_y, hw_z
--- src/data/test.py
+++ src/data/test.py
@@ -3,8 +3,23 @@ import numpy as np
 import sys
 np.set_printoptions(threshold=sys.maxsize)
 from PIL import Image, ImageFont, ImageDraw
-from heatmap import gen_3d_heatmap
+from heatmap import gen_3d_heatmap, gen_3d_hw
 from abus_data import AbusNpyFormat
+
+def draw_slice(volume, dir, label=None):
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+    min, max = np.min(volume), np.max(volume)
+    volume = ((volume-min)/max)*255
+    for i in range(np.shape(volume)[1]):
+        img = Image.fromarray(volume[:,i,:].astype(np.uint8), 'L')
+        img = img.convert(mode='RGB')
+        draw = ImageDraw.Draw(img)
+        if label != None:
+            for bx in label:
+                if bx['y_center']//4== i:
+                    draw.point([(int(bx['x_center']//4),int(bx['z_center']//4))],fill="red")
+        img.save(os.path.join(dir ,(str(i)+'.png')))
 
 def main():
     all_data = AbusNpyFormat(root, train=False, validation=False)
@@ -12,26 +27,25 @@ def main():
     print('Dataset size:', all_data.__len__())
     print('Shape of data:', data.size())
     print('Number of boxes in data:', len(label))
-    #print('Volumetric tensor:', data)
-    #print('Label:', label)
 
     scale=4
 
-    hm1 = gen_3d_heatmap((640,160,640), label, scale)
-    hm1 = np.transpose(hm1, (2,1,0))
-    min, max = np.min(hm1), np.max(hm1)
-    hm1 = ((hm1-min)/max)*255
-    for i in range(np.shape(hm1)[1]):
-        img = Image.fromarray(hm1[:,i,:].astype(np.uint8), 'L')
-        img = img.convert(mode='RGB')
-        draw = ImageDraw.Draw(img)
-        for bx in label:
-            if bx['y_center']//scale == i:
-                draw.point([(int(bx['z_center']//scale),int(bx['x_center']//scale))],fill="red")
-        img.save(os.path.join(os.path.dirname(__file__),(str(i)+'.png')))
+    hm = gen_3d_heatmap((640,160,640), label, scale)
+    #hm = np.transpose(hm, (2,1,0))
+    tmp_dir = os.path.join(os.path.dirname(__file__),'test','hm')
+    draw_slice(hm, tmp_dir, label)
+
+    hw_x, hw_y, hw_z = gen_3d_hw((640,160,640), label, scale)
+    tmp_dir = os.path.join(os.path.dirname(__file__),'test','hw_x')
+    draw_slice(hw_x, tmp_dir)
+    tmp_dir = os.path.join(os.path.dirname(__file__),'test','hw_y')
+    draw_slice(hw_y, tmp_dir)
+    tmp_dir = os.path.join(os.path.dirname(__file__),'test','hw_z')
+    draw_slice(hw_z, tmp_dir)
 
     return
 
 if __name__ == '__main__':
     root = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__)))), 'data/sys_ucc/')
+
     main()
