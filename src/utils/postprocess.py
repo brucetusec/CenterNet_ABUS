05e0491475f3cb3a8d591a1cd0028d01ac781820
r07922097 <r07922097@ntu.edu.tw>
2020-05-04 16:03:39 +0800

    Distance as hit criteria

--- src/utils/postprocess.py
+++ src/utils/postprocess.py
@@ -69,3 +69,47 @@ def eval_precision_recall(pred_BB, true_BB, det_thresh, scale):
     FP = len(pred_hits) - pred_hits.sum()
     FN = len(true_BB)-gt_hits.sum()
     return int(TP),int(FP),int(FN),hits_index,hits_iou,hits_score
+
+
+def centroid_distance(box1, box2):
+    b1_z0, b1_y0, b1_x0, b1_z1, b1_y1, b1_x1 = box1
+    b2_z0, b2_y0, b2_x0, b2_z1, b2_y1, b2_x1 = box2
+
+    b1_z0, b1_y0, b1_x0, b1_z1, b1_y1, b1_x1 = int(b1_z0), int(b1_y0), int(b1_x0), int(b1_z1), int(b1_y1), int(b1_x1)
+    b2_z0, b2_y0, b2_x0, b2_z1, b2_y1, b2_x1 = int(b2_z0), int(b2_y0), int(b2_x0), int(b2_z1), int(b2_y1), int(b2_x1)
+    
+    b1_centroid_z, b1_centroid_y, b1_centroid_x = (b1_z1+b1_z0)/2, (b1_y1+b1_y0)/2, (b1_x1+b1_x0)/2
+    b2_centroid_z, b2_centroid_y, b2_centroid_x = (b2_z1+b2_z0)/2, (b2_y1+b2_y0)/2, (b2_x1+b2_x0)/2
+
+    b1_centroid = np.array([b1_centroid_z, b1_centroid_y, b1_centroid_x])
+    b2_centroid = np.array([b2_centroid_z, b2_centroid_y, b2_centroid_x])
+
+    dist = np.linalg.norm(b1_centroid - b2_centroid)
+    
+    return dist
+
+
+def eval_precision_recall_by_dist(pred_BB, pred_score, true_BB, dist_thresh):
+
+    pred_hits = np.zeros(len(pred_BB))
+    gt_hits = np.zeros(len(true_BB))
+    hits_index = -np.ones(len(true_BB))
+    hits_iou = np.zeros(len(true_BB), dtype=float)
+    hits_score = np.zeros(len(true_BB), dtype=float)
+
+    for pred_idx, pred_bb in enumerate(pred_BB):
+
+        for gt_idx, gt_roi in enumerate(true_BB):
+            
+            dist = centroid_distance(pred_bb[:6], gt_roi[:6])
+            if dist <= dist_thresh:
+                gt_hits[gt_idx] = 1
+                hits_index[gt_idx] = pred_idx
+                hits_iou[gt_idx] = dist
+                hits_score[gt_idx] = pred_score[pred_idx]
+                pred_hits[pred_idx] = 1
+
+    TP = gt_hits.sum()
+    FP = len(pred_hits) - pred_hits.sum()
+    FN = len(true_BB)-gt_hits.sum()
+    return int(TP), int(FP), int(FN), hits_index, hits_iou, hits_score
